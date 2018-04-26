916b8da0f61980f0ae4d5bd2a22c192d2fd467f7
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-04-26 16:48:18 +0900

    Write db.channels.create
    Co-authored by Mike Donnelly <sqldonnelly@gmail.com>
    Co-authored by Alex Ogilvie <alexander.ogilvie@icloud.com>

--- services/db/channels/create.js
+++ services/db/channels/create.js
@@ -1,8 +1,32 @@
+const Promise = require("bluebird");
+
 const validateChannelName = (uName) =>
   typeof uName === "string" && uName.replace(" ", "").length > 3;
 
 module.exports = (knex, Channel) => {
   return (params) => {
-    return Promise.resolve({}); // fix me!
+    const channelName = params.name;
+
+    return Promise.try(() => {
+      if (!validateChannelName(channelName))
+        throw new Error(
+          "Channel name must be provided, and be at least three characters"
+        );
+    })
+      .then(() => knex("channels").insert({ name: channelName.toLowerCase() }))
+      .then(() => {
+        return knex("channels")
+          .where({ name: channelName.toLowerCase() })
+          .select();
+      })
+      .then((channels) => new Channel(channels.pop())) // create a user model out of the plain database response
+      .catch((err) => {
+        // sanitize known errors
+        if (err.message.match("duplicate key value"))
+          throw new Error("That channel already exists");
+
+        // throw unknown errors
+        throw err;
+      });
   };
 };
