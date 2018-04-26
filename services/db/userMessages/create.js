682cda57bf2f57f6bf305d795733518db4b4b715
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-04-26 18:30:31 +0900

    Write db.usermessages.create
    Co-authored by Mike Donnelly <sqldonnelly@gmail.com>
    Co-authored by Alex Ogilvie <alexander.ogilvie@icloud.com>

--- services/db/userMessages/create.js
+++ services/db/userMessages/create.js
@@ -1,5 +1,36 @@
 module.exports = (knex, UserMessage) => {
   return (params) => {
-    return Promise.resolve({}); // fix me!
+    const fromId = params.fromId;
+    const toId = params.toId;
+    const message = params.message;
+
+    return knex("user_messages")
+      .insert({
+        from_id: fromId,
+        to_id: toId,
+        message,
+      })
+      .then(() => {
+        return knex("user_messages")
+          .join(
+            "users as userFrom",
+            "user_messages.from_id",
+            "=",
+            "userFrom.id"
+          )
+          .join("users as userTo", "user_messages.to_id", "=", "userTo.id")
+          .select(
+            "user_messages.id",
+            "userFrom.username as from",
+            "message",
+            "sent_at"
+          )
+          .where("userFrom.id", fromId);
+      })
+      .then((userMessages) => {
+        return userMessages.map((message) => {
+          return new UserMessage(message);
+        });
+      });
   };
 };
