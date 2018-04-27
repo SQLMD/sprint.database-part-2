fa2396c329aeb45dcddc034721a41e731899db90
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-04-27 11:44:19 +0900

    Write db.user_messages.list
    Co-authored by Mike Donnelly <sqldonnelly@gmail.com>
    Co-authored by Alex Ogilvie <alexander.ogilvie@icloud.com>

--- services/db/userMessages/list.js
+++ services/db/userMessages/list.js
@@ -1,5 +1,30 @@
 module.exports = (knex, UserMessage) => {
   return (params) => {
-    return Promise.resolve([]); // fix me!
+    const fromId = Number(params.fromId);
+    const toId = Number(params.toId);
+
+    return knex("user_messages")
+      .join("users as fromUser", "fromUser.id", "=", "user_messages.from_id")
+      .join("users as toUser", "toUser.id", "=", "user_messages.to_id")
+      .select(
+        "fromUser.username as from",
+        "user_messages.message",
+        "user_messages.id",
+        "user_messages.sent_at"
+      )
+      .where({
+        "user_messages.from_id": fromId,
+        "user_messages.to_id": toId,
+      })
+      .orWhere({
+        "user_messages.from_id": toId,
+        "user_messages.to_id": fromId,
+      })
+
+      .then((userMessages) => {
+        return userMessages.map((message) => {
+          return new UserMessage(message);
+        });
+      });
   };
 };
